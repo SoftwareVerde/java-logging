3b9e0888804b8234dbdb12ba48d7bc5a7524262f
Josh Green <josh@softwareverde.com>
2019-08-14 21:05:56 -0400

    FIX: StackTrace depth calculation now assumes invoker's context.

--- src/main/java/com/softwareverde/logging/StackTraceManager.java
+++ src/main/java/com/softwareverde/logging/StackTraceManager.java
@@ -23,17 +23,20 @@ class StackTraceManager extends java.lang.SecurityManager {
 
     /**
      * Returns the call-depth of the first non-com.softwareverde.logging class invocation.
+     *  The returned depth is from the perspective of the invoker of this function, not from within this function.
      */
     public final Integer getCallingDepth() {
         final Class<?>[] callingClasses = super.getClassContext();
-        for (int i = 0; i < callingClasses.length; ++i) {
+        if (callingClasses.length < 2) { return 0; }
+
+        for (int i = 1; i < callingClasses.length; ++i) {
             final Class<?> callingClass = callingClasses[i];
             final String packageName = Package.getClassName(callingClass);
             if (! packageName.startsWith(LOGGING_PACKAGE_NAME)) {
-                return i;
+                return (i - 1);
             }
         }
 
-        return (callingClasses.length - 1);
+        return (callingClasses.length - 2);
     }
 }
