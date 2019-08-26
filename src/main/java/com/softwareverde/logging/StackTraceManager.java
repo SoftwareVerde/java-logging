99a4bd3440690a8908ef74f05dbd1748c2643e5e
Josh Green <josh@softwareverde.com>
2019-08-26 15:03:53 -0400

    Updated StackTraceManager for Android support.

--- src/main/java/com/softwareverde/logging/StackTraceManager.java
+++ src/main/java/com/softwareverde/logging/StackTraceManager.java
@@ -5,11 +5,38 @@ import com.softwareverde.util.Package;
 class StackTraceManager extends java.lang.SecurityManager {
     public static final String LOGGING_PACKAGE_NAME = "com.softwareverde.logging";
 
+    protected Class<?>[] _getCallingClasses() {
+        { // Default case...
+            final Class<?>[] callingClasses = super.getClassContext();
+            if (callingClasses != null) { return callingClasses; }
+        }
+
+        final Exception exception = new Exception();
+        final StackTraceElement[] stackTraceElements = exception.getStackTrace();
+        final Class<?>[] callingClasses = new Class<?>[stackTraceElements.length];
+
+        for (int i = 0; i < stackTraceElements.length; ++i) {
+            final StackTraceElement stackTraceElement = stackTraceElements[i];
+            final String className = stackTraceElement.getClassName();
+            try {
+                final Class<?> clazz = Class.forName(className);
+                callingClasses[i] = clazz;
+            }
+            catch (final Exception classNotFoundException) {
+                System.err.println("Log Error: Class not found: " + className);
+                callingClasses[i] = null;
+            }
+        }
+
+        return callingClasses;
+    }
+
     /**
      * Returns the first non-com.softwareverde.logging class invocation.
      */
     public final Class<?> getCallingClass() {
-        final Class<?>[] callingClasses = super.getClassContext();
+        final Class<?>[] callingClasses = _getCallingClasses();
+
         for (int i = 0; i < callingClasses.length; ++i) {
             final Class<?> callingClass = callingClasses[i];
             final String packageName = Package.getClassName(callingClass);
